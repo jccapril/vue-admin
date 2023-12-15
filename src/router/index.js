ebef6633ef8bd6035928b8604e3eeb25fd94b3ee
Azir <azir@AzirdeMacBook-Pro.local>
2023-12-15 22:07:39 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -55,108 +55,82 @@ export const constantRoutes = [
     }]
   },
 
+
+
   {
-    path: '/example',
+    path: '/user',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
+    redirect: '/user/list',
+    alwaysShow: true,
+    name: 'User',
+    meta: { title: '用户管理', icon: 'peoples' },
     children: [
       {
-        path: 'table',
-        name: 'Table',
+        path: 'list',
+        name: 'List',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: '用户列表', icon: 'list' }
       },
       {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'follow',
+        name: 'Follow',
+        component: () => import('@/views/table/index'),
+        meta: { title: '关注管理', icon: 'list' }
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
+        path: 'fan',
+        name: 'Fan',
+        component: () => import('@/views/table/index'),
+        meta: { title: '粉丝管理', icon: 'list' }
       }
+
     ]
   },
 
   {
-    path: 'external-link',
+    path: '/post',
     component: Layout,
+    redirect: '/post/list',
+    name: 'Post',
+    meta: { title: '微博管理', icon: 'form' },
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'list',
+        name: 'List',
+        component: () => import('@/views/table/index'),
+        meta: { title: '微博列表', icon: 'list' }
+      },
+      {
+        path: 'tag',
+        name: 'Tag',
+        component: () => import('@/views/table/index'),
+        meta: { title: '话题列表', icon: 'list' }
+      },
+      {
+        path: 'hot',
+        name: 'Hot',
+        component: () => import('@/views/table/index'),
+        meta: { title: '热搜管理', icon: 'list' }
       }
+
+    ]
+  },
+
+  {
+    path: '/log',
+    component: Layout,
+    redirect: '/log/list',
+    alwaysShow: true,
+    name: 'Log',
+    meta: { title: '日志管理', icon: 'list' },
+    children: [
+      {
+        path: 'list',
+        name: 'List',
+        component: () => import('@/views/table/index'),
+        meta: { title: '日志列表', icon: 'list' }
+      }
+
     ]
   },
 
